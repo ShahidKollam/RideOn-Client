7ab5bc0c1ea1dd9306209db12f98fc3fe458faa1
shahid <shahidazhari313@gmail.com>
2026-07-18 22:50:41 +0530

    Add pino-pretty

--- Server/src/config/logger.js
+++ Server/src/config/logger.js
@@ -1,25 +1,21 @@
 import pino from 'pino'
 
-// const logger = pino({
-//     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-//     transport: {
-//         target: 'pino-pretty',
-//         options: {
-//             colorize: true,
-//         },
-//     },
-// })
-
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      singleLine: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname,req,res',
-    },
-  },
-});
+const logger =
+    process.env.NODE_ENV === 'production'
+        ? pino({
+              level: 'info',
+          })
+        : pino({
+              level: 'debug',
+              transport: {
+                  target: 'pino-pretty',
+                  options: {
+                      colorize: true,
+                      singleLine: true,
+                      translateTime: 'HH:MM:ss',
+                      ignore: 'pid,hostname,req,res',
+                  },
+              },
+          })
 
 export default logger
