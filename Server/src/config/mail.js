76883059ad0d8372eaba556dda2f46d05ca6a987
shahid <shahidazhari313@gmail.com>
2026-07-19 16:52:27 +0530

    FIX 1

--- Server/src/config/mail.js
+++ Server/src/config/mail.js
@@ -1,14 +1,35 @@
-import nodemailer from 'nodemailer';
-import { config } from './env.js';
+// import nodemailer from 'nodemailer';
+// import { config } from './env.js';
+
+// const transporter = nodemailer.createTransport({
+//   host: config.mailHost,
+//   port: config.mailPort,
+//   secure: false,
+//   auth: {
+//     user: config.mailUser,
+//     pass: config.mailPass,
+//   },
+// });
+
+// export default transporter;
+import nodemailer from 'nodemailer'
+import { config } from './env.js'
 
 const transporter = nodemailer.createTransport({
-  host: config.mailHost,
-  port: config.mailPort,
-  secure: false,
-  auth: {
-    user: config.mailUser,
-    pass: config.mailPass,
-  },
-});
+    host: config.mailHost,
+    port: Number(config.mailPort),
+    secure: false, // Port 587
+    auth: {
+        user: config.mailUser,
+        pass: config.mailPass,
+    },
 
-export default transporter;
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+
+    logger: true,
+    debug: true,
+})
+
+export default transporter
